c2452038a551330c7ae024bc54322d5728da4d3d
Navaneethb12 <navaneethb12@gmail.com>
2025-03-11 11:07:05 +0530

    Update install-tomcat-ubuntu.sh

--- install-tomcat-ubuntu.sh
+++ install-tomcat-ubuntu.sh
@@ -2,7 +2,7 @@
 
 echo "\n################################################################"
 echo "#                                                              #"
-echo "#                     ***Artisan Tek***                        #"
+echo "#                     ***Navaneeth B***                        #"
 echo "#                    Tomcat Installation                       #"
 echo "#                                                              #"
 echo "################################################################"
@@ -14,22 +14,22 @@ sudo apt-get install -y default-jre 1>/dev/null
 echo "            -> Done"
 
 # Downloading Apache Tomcat 9.0.68 version to OPT folder
-echo "*****Downloading Apache Tomcat 9.0.68 version"
+echo "*****Downloading Apache Tomcat 9.0.102 version"
 cd /opt
 sudo systemctl stop tomcat 2>/dev/null
 sudo rm -rf apache* tomcat*
 sudo mkdir -p /opt/tomcat
-sudo wget -q https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.68/bin/apache-tomcat-9.0.68.tar.gz
-sudo tar xf apache-tomcat-9.0.68.tar.gz -C /opt/tomcat 1>/dev/null
-sudo rm -rf apache-tomcat-9.0.68.tar.gz
+sudo wget -q https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.102/bin/apache-tomcat-9.0.102.tar.gz
+sudo tar xf apache-tomcat-9.0.102.tar.gz -C /opt/tomcat 1>/dev/null
+sudo rm -rf apache-tomcat-9.0.102.tar.gz
 echo "            -> Done"
 
 # Configuring Tomcat server for manager, host-manager and tomcat users
 echo "*****Configuring Tomcat server for Manager, Host-manager and Credentials"
 cd - 1>/dev/null
-sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.68/webapps/manager/META-INF/context.xml
-sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.68/webapps/host-manager/META-INF/context.xml
-sudo cp tomcat-users.xml /opt/tomcat/apache-tomcat-9.0.68/conf/tomcat-users.xml
+sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.102/webapps/manager/META-INF/context.xml
+sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.102/webapps/host-manager/META-INF/context.xml
+sudo cp tomcat-users.xml /opt/tomcat/apache-tomcat-9.0.102/conf/tomcat-users.xml
 echo "            -> Done"
 
 # Configuring Tomcat as a Service
